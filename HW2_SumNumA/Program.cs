1e937f49fda0c55b6f2e63a573599ee72cb186f4
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-23 10:35:42 +0300

    HW2_SumNumA

---
+++ HW2_SumNumA/Program.cs
@@ -0,0 +1,16 @@
+﻿// написать метод, который вычисляет
+// сумму цифр в числе A
+Console.WriteLine("Введите число А");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSunNumA()
+{
+  int SunNumA = 0;
+  while (num > 0)
+  {
+    SunNumA = SunNumA + num % 10;
+    num /= 10;
+  }
+  return SunNumA;
+}
+Console.WriteLine(GetSunNumA());
