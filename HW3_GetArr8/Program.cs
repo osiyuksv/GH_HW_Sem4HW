843391f6d029a8179f66825252e30b3f336342d8
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-23 11:05:49 +0300

    HW3_GetArr8

---
+++ HW3_GetArr8/Program.cs
@@ -0,0 +1,14 @@
+﻿// метод, который создаёт массив из 8 двухзначных (1-99)
+// рандомных элементов и выводит на экран эелемнты через ", "
+
+int[] arr00 = new int[8];
+
+int[] GetArrRand00()
+{
+  for (int i = 0; i < arr00.Length; i++)
+  {
+    arr00[i] = new Random().Next(1, 100);
+  }
+  return arr00;
+}
+Console.Write(String.Join((", "), GetArrRand00()));
