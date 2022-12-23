e3dce5ae0e14a7a871d9c742a5a0bd3b2dec885f
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-23 09:59:44 +0300

    HW1_APowB

---
+++ HW1_APowB/Program.cs
@@ -0,0 +1,14 @@
+﻿// написать метод, который возвотит
+// число A в натуральную степень B
+Console.WriteLine("Введите число А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+double GetApowB()
+{
+  double numApowB = Math.Pow(numA, numB);
+  return numApowB;
+}
+Console.WriteLine(GetApowB());
+
